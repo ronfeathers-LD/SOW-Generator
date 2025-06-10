4d11859f8c6416cc122e31c5175450061ee4b525
Ron Feathers <ron@webfeathers.com>
2025-06-10 10:07:55 -0700

    Add Google image domain to Next.js config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
       allowedOrigins: ['localhost:3000', 'sow-generator-one.vercel.app'],
     },
   },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
 }
 
 module.exports = nextConfig 
